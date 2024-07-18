d7eededf77d55670dd5dff25e85028b78aae5d76
Cole Blender <coleblender@gmail.com>
2024-07-18 15:07:05 -0700

    add sitemap

--- src/app/artists/[id]/page.tsx
+++ src/app/artists/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { Artist } from "@/artists";
+import { Artist, artists } from "@/artists";
+import { Metadata } from "next";
+// import { cache } from "react";
 
 type Props = {
   params: { id: string };
@@ -6,6 +8,37 @@ type Props = {
     count: string;
   };
 };
+
+// const fetchData = cache(() => {
+
+// })
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
+  const { id } = params;
+  const { count } = searchParams;
+
+  const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/api/artists/`);
+  url.searchParams.set("id", id);
+  if (count) {
+    url.searchParams.set("count", count);
+  }
+  const { href: route } = url;
+
+  const artist: Artist | null = await fetch(route).then((res) => res.json());
+
+  return {
+    title: `${artist?.name || "Product Details"}`,
+  };
+}
+
+export async function generateStaticParams() {
+  return artists.map((artist) => ({
+    id: artist.id,
+  }));
+}
 
 async function ArtistPage({ params, searchParams }: Props) {
   const { id } = params;
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const title = "My Website";
 
 export const description =
-  "SuperlativeSites offers top-notch web development services using Next.js, React, and Supabase. Based in Las Vegas, we create stunning, high-performance websites that drive results. Elevate your online presence with cutting-edge technology and expert design. Contact us today to transform your vision into reality!";
+  "I need to generate a very good description to put in my head tag for my website. I want the best SEO in the world. My website...               . Make sure this description helps my website get to the top of Google search by using world class SEO. Just simply give me the text.";
