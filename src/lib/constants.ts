7d4cd7bfa320a890cf004e834129a363736edc0a
Cole Blender <coleblender@gmail.com>
2024-07-18 14:40:24 -0700

    push

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const title = "My Website";
+
+export const description =
+  "SuperlativeSites offers top-notch web development services using Next.js, React, and Supabase. Based in Las Vegas, we create stunning, high-performance websites that drive results. Elevate your online presence with cutting-edge technology and expert design. Contact us today to transform your vision into reality!";
